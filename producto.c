0bcd9cf97b105b45256e47f4b0167001f8911248
Martin Burga <martin,burga@udla.edu.ec>
2024-01-24 16:11:44 -0500

    IV): Se ha agregado la funcionalidad completa de toda la segunda opcion de visualizar los productos realizados. Se ha reinterpretado la variable MARCA y se la renombro como descripcion. Se han agregado espacios de estetica y se ha reescrito algunos versos del codigo.

--- producto.c
+++ producto.c
@@ -6,22 +6,23 @@ struct producto
 {
     char codigo[25];
     char nombreproducto[50];
-    char marca[50];
+    char descripcion[50];
     int cantidad;
     float preciocompra;
 };
 
 int main()
 {
-    printf("Bienvenido al registro de productos.\n");
+    printf("\nBienvenido al registro de productos del Supermercado.\n");
+    printf("\n");
     FILE *registro;
     struct producto n;
 
     do
     {
-        printf("Por favor, elige una opcion a realizar.\n");
-        printf("1. Ingresar informacion del producto.\n");
-        printf("2. Imprimir informacion de los productos.\n");
+        printf("Elige una opcion a realizar.\n");
+        printf("1. Registrar informacion de un nuevo producto.\n");
+        printf("2. Ver la lista de los productos registrados.\n");
         printf("3. Salir.\n");
         scanf("%d", &opcion);
         printf("\n");
@@ -35,24 +36,42 @@ int main()
                 return 1;
             }
 
-            printf("Bienvenido al registro de los productos.\n");
+            printf("Se procedera a registrar la informacion de un nuevo producto.\n");
             printf("Por favor, inserta el codigo del producto: ");
             scanf("%s", n.codigo);
             printf("Por favor, inserta el nombre del producto: ");
             scanf("%s", n.nombreproducto);
-            printf("Por favor, inserta la marca del producto: ");
-            scanf("%s", n.marca);
-            printf("Por favor, inserta la cantidad que hay del producto: ");
+            printf("Por favor, inserta una breve descripcion dle producto\n");
+            scanf("%s", n.descripcion);
+            printf("Por favor, inserta la cantidad que hay del producto:");
             scanf("%d", &n.cantidad);
             printf("Por favor, inserta el precio del producto: ");
             scanf("%f", &n.preciocompra);
-            printf("Exito! El producto ha sido registrado.\n");
-            fprintf(registro, "%s\t%s\t%s\t%d\t%.2f\n", n.codigo, n.nombreproducto, n.marca, n.cantidad, n.preciocompra);
+            printf("\n");
+            printf("El producto ha sido registrado exitosamente.\n");
+            printf("\n");
+            fprintf(registro, "%s\t%s\t%s\t%d\t%.2f\n", n.codigo, n.nombreproducto, n.descripcion, n.cantidad, n.preciocompra);
             fclose(registro);
         }
         else if (opcion == 2)
         {
-            printf("Los productos ingresados son:\n");
+            registro = fopen("archivo.txt", "r");
+            if (registro == NULL)
+            {
+                printf("Se ha producido un error al intentar abrir el archivo.\n");
+                return 1;
+            }
+            printf("Los productos registrados en el archivo son:\n");
+            printf("Codigo\tNombre\tMarca\tCantidad\tPrecioCompra\n");
+
+            while (fscanf(registro, "%s %s %s %d %f", n.codigo, n.nombreproducto, n.descripcion, &n.cantidad, &n.preciocompra) != EOF) 
+            //La funcion EOF permite al scanf detectar cuando se llega a la final de una archivo. Resumidamente, en la anterior linea dice que 
+            //mientras exista datos registrados y no haya el fineal del archivo, se imprimira la n cantidad de productos ingresados por le usuario.
+            {
+                printf("%s\t%s\t%s\t%d\t%.2f\n", n.codigo, n.nombreproducto, n.descripcion, n.cantidad, n.preciocompra);
+            }
+            printf("\n");
+            fclose(registro);
         }
 
         else if (opcion != 3)
@@ -64,6 +83,7 @@ int main()
     } while (opcion != 3);
 
     printf("Gracias por utilizar el programa.\n");
+    printf("\n");
 
     return 0;
 }
