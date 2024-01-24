bc4faeba377618bf1e2bf4aa32ab045aef6c7eed
Martin Burga <martin,burga@udla.edu.ec>
2024-01-24 15:29:15 -0500

    II): Se ha avanzado la opcion 1 en la que el usuario crea un archivo txt al momento de presionar esta opcion. En Caso de no haber un archivo, lo crea. En caso de haberlo, se sobreescribe. Se ha añadido controladores ante digitacion de opciones fueras de lo previsto y se ha corregido un error de impresion

--- producto.c
+++ producto.c
@@ -21,14 +21,38 @@ int main()
         printf("2. Imprimir informacion de los productos.\n");
         printf("3. Salir.\n");
         scanf("%d", &opcion);
+        printf("\n");
+
+        if (opcion == 1)
+        {
+            FILE *registro;
+            registro = fopen("archivo.txt", "w");
+            if (registro == NULL)
+            {
+                printf("Se ha producido un error al intentar abrir el archivo\n");
+                return 1;
+            }
+
+            printf("Bienvenido al registro de los productos.\n");
+            printf("Por favor, inserta el nombre del producto\n");
+        }
+        else if (opcion == 2)
+        {
+            printf("Los productos que han sido registrados son: \n");
+        }
+
+        if (opcion != 1 && opcion != 2 && opcion!=3)
+        {
+            printf("No existe la opcion insertada. Por favor, vuelve a intentarlo.\n");
+            printf("\n");
+        }
 
     } while (opcion != 3);
 
-    if (opcion==3)
+    if (opcion == 3)
     {
         printf("Gracias por utilizar el programa.\n");
     }
-    
 
     return 0;
 }
